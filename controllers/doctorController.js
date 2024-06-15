06fec8d5dbd310cde7b786dfd560358d4b7ef10e
Islam Abdelmajeed <islam.abdelmajeed2@gmail.com>
2024-06-15 08:49:41 +0300

    add filter

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -1,9 +1,9 @@
 import Doctor from '../models/Doctor.js';
 
-// Search doctors by name, specialty, or address
-export const searchDoctorsForPatient = async (req, res) => {
+// Search and filter doctors by various criteria
+export const searchAndFilterDoctors = async (req, res) => {
   try {
-    const { name, specialty, address } = req.query;
+    const { name, specialty, address, priceMin, priceMax, place, date, time } = req.query;
 
     // Build the search query object
     const query = {};
@@ -26,6 +26,33 @@ export const searchDoctorsForPatient = async (req, res) => {
         { 'address.state': { $regex: address, $options: 'i' } },
         { 'address.zipCode': { $regex: address, $options: 'i' } }
       ];
+    }
+
+    if (priceMin || priceMax) {
+      // Filter by price range
+      query.price = {};
+      if (priceMin) query.price.$gte = Number(priceMin);
+      if (priceMax) query.price.$lte = Number(priceMax);
+    }
+
+    if (place) {
+      // Filter by place availability (Clinic or Home)
+      if (place.toLowerCase() === 'clinic') {
+        query.clinicAvailability = true;
+      } else if (place.toLowerCase() === 'home') {
+        query.homeVisitAvailability = true;
+      }
+    }
+
+    if (date && time) {
+      // Filter by availability on a specific date and time
+      const timeFormatRegex = /^(0[1-9]|1[0-2]):[0-5][0-9] (AM|PM)$/;
+      if (!timeFormatRegex.test(time)) {
+        return res.status(400).json({ message: 'Time must be in the format "HH:MM AM/PM"' });
+      }
+      query.availableTimes = time;
+      // Note: Here we're assuming doctors have fixed available times regardless of date.
+      // To filter by specific dates, you would need to include a more complex scheduling system in your model.
     }
 
     // Find doctors that match the query
