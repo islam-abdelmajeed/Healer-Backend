59060d0027e465fea341c00f26fd7a5fc9af9e1f
Islam Abdelmajeed <islam.abdelmajeed2@gmail.com>
2024-06-15 08:51:52 +0300

    fix: filter

--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import authMiddleware from '../middleware/authMiddleware.js';
 import { bookAppointment, getPatientAppointments } from '../controllers/bookingController.js';
-import { searchDoctorsForPatient } from '../controllers/doctorController.js'; // Import the search controller for patients
+import { searchAndFilterDoctors } from '../controllers/doctorController.js'; // Import the search controller for patients
 
 
 const router = express.Router();
 
 router.post('/book', authMiddleware('patient'), bookAppointment);
 router.get('/appointments', authMiddleware('patient'), getPatientAppointments);
-router.get('/search/doctors', authMiddleware('patient'), searchDoctorsForPatient);
+router.get('/search/doctors', authMiddleware('patient'), searchAndFilterDoctors);
 
 export default router;
