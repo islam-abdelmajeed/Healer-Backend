fb5c41695977df3879ac4ec211dc7a9873f7812f
hp <kareemtall99@gmail.com>
2024-06-29 16:42:37 +0300

    adding cloud

--- controllers/documentController.js
+++ controllers/documentController.js
@@ -1,4 +1,5 @@
 import Doctor from '../models/Doctor.js';
+import cloudinary from '../utils/cloudinary.js';
 
 export const uploadDocuments = async (req, res) => {
   try {
@@ -15,6 +16,37 @@ export const uploadDocuments = async (req, res) => {
     }
     if (req.files.insurance) {
       doctor.insuranceDocument = req.files.insurance[0].path;
+    }
+
+    doctor.isDocumentsUploaded = !!(doctor.licenseDocument && doctor.insuranceDocument);
+    await doctor.save();
+
+    res.status(200).json({ message: 'Documents uploaded successfully', doctor });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+export const uploadDocumentsV2 = async (req, res) => {
+  try {
+    const { id } = req.user; // Assuming user ID is set in req.user by authMiddleware
+    const doctor = await Doctor.findById(id);
+
+    if (!doctor) {
+      return res.status(404).json({ message: 'Doctor not found' });
+    }
+
+    // Save file paths to the doctor's document fields
+    if (req.files.license) {
+      const { secure_url } = await cloudinary.uploader.upload(
+        req.files.license[0].path,);    
+        doctor.licenseDocument = secure_url;
+      }
+    if (req.files.insurance) {
+      const { secure_url } = await cloudinary.uploader.upload(
+        req.files.insurance[0].path,);   
+      doctor.insuranceDocument = secure_url
+
+
     }
 
     doctor.isDocumentsUploaded = !!(doctor.licenseDocument && doctor.insuranceDocument);
--- routes/documentRoutes.js
+++ routes/documentRoutes.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import multer from 'multer';
 import authMiddleware from '../middleware/authMiddleware.js';
-import { uploadDocuments } from '../controllers/documentController.js';
+import { uploadDocuments, uploadDocumentsV2 } from '../controllers/documentController.js';
+import { fileUpload, fileValidation } from '../utils/multerCloud.js';
 
 const router = express.Router();
 
@@ -17,5 +18,6 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 router.post('/upload', authMiddleware('doctor'), upload.fields([{ name: 'license' }, { name: 'insurance' }]), uploadDocuments);
+router.post('/uploadv2', authMiddleware('doctor'),fileUpload([...fileValidation.image]).fields([{ name: 'license' }, { name: 'insurance' }]) , uploadDocumentsV2);
 
 export default router;
